5237267ea2c8322cea6ace0b2708d8a468801ede
Asef00 <asefhosseini00@gmail.com>
2024-09-25 13:01:35 +0330

    implement basic mouse tracker element

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import useMousePosition from './components/Mouse'
+
 function App() {
+  const mousePosition = useMousePosition()
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello, World!</h1>
+      <div
+        className="absolute p-10 bg-slate-500/80 rounded-lg text-white"
+        style={{ top: mousePosition.y, left: mousePosition.x }}
+      >
+        <h1 className="text-3xl font-bold underline">Hello, World!</h1>
+      </div>
+
+      <p>
+        Your cursor position:
+        <br />
+        {JSON.stringify(mousePosition)}
+      </p>
     </>
   )
 }
